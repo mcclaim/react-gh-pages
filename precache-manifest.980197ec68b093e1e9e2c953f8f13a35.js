a3a89cbba50af09ea7512cc690e429a162a20598
faruh <faruh@mail.ru>
2019-11-05 12:04:58 +0500

    Updates

---
+++ precache-manifest.980197ec68b093e1e9e2c953f8f13a35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ffdace2e3cc87a049c0007e570f6af6",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "7abe861799b27e7037bd",
+    "url": "/react-gh-pages/static/css/main.ab9c8440.chunk.css"
+  },
+  {
+    "revision": "aeac74c4f50dd037dfa3",
+    "url": "/react-gh-pages/static/js/2.1c01e602.chunk.js"
+  },
+  {
+    "revision": "7abe861799b27e7037bd",
+    "url": "/react-gh-pages/static/js/main.024c371c.chunk.js"
+  },
+  {
+    "revision": "91b824cb3849a06601c0",
+    "url": "/react-gh-pages/static/js/runtime-main.28988767.js"
+  }
+]);
